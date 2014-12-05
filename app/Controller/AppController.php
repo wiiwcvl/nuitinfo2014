2a0be836ea567be18dadccb9ffed02c75059abdc
aymeric berquin <aymeric.berquin@insa-cvl.fr>
2014-12-05 02:37:52 +0100

    modif sous ordre du chef

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -58,7 +58,7 @@ class AppController extends Controller {
 	);
 
 	public function beforeFilter() {
-		$this->Auth->allow('index', 'view');
+		$this->Auth->allow('index', 'view','display');
 }
 	public $theme = "Cakestrap";
 }
